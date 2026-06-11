dcc3a82b42ef785f398af789c1c33c606f380fa4
Vasutiwari1 <25BCH122@spt.pdpu.ac.in>
2026-06-11 20:39:14 +0530

    Update antoine_vapor_pressure.py

--- antoine_vapor_pressure.py
+++ antoine_vapor_pressure.py
@@ -1,8 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 temperatures = np.array([60, 70, 80, 90, 100])
 A = 8.07131
 B = 1730.63
 C = 233.426
 P_star = 10 ** (A - B/(C + temperatures))
-for i in range(len(temperatures)):
-    print(f"T={temperatures[i]}°C → P*={P_star[i]:.2f} mmHg")
+plt.plot(temperatures,P_star,
+         color='red',
+         marker='s',
+         )
+plt.xlabel("TEMPRATURE")
+plt.ylabel("WATER VAPOUR PRESSURE")
+plt.title("Antoine Equation — Water Vapor Pressure")
+plt.grid(True)  
+plt.savefig('antoine_graph.png')
+plt.show()
+
